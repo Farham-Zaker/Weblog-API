d6b617e9b159d71f49c2c9edb1753d51e75b50dc
Farham Zaker <farham.zaker007@gmail.com>
2025-06-09 11:26:41 -1200

    feat(comment): add '/comment/create' route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\isLogged;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 use App\Http\Middleware\CheckArticleWriter;
+use App\Http\Controllers\CommentController;
 
 Route::prefix("auth")->group(function () {
     Route::post("/register", [UserController::class, "register"]);
@@ -18,4 +19,8 @@ Route::prefix("article")->group(function () {
     Route::get("/public", [ArticleController::class, "public"]);
     Route::get("/get/{article_id}", [ArticleController::class, "getById"]);
     Route::middleware(CheckArticleWriter::class)->put("/update", [ArticleController::class, "update"]);
+});
+
+Route::prefix("comment")->group(function () {
+    Route::middleware(isLogged::class)->post("/create");
 });
