4c29599e3d45557c727379a144036c8677255c4e
Farham Zaker <farham.zaker007@gmail.com>
2025-06-08 12:33:04 -1200

    feat(article): add function to article repo for gettin article by user id

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -4,8 +4,14 @@ namespace App\Repositories;
 
 use App\Models\Article;
 
-class ArticleRepository {
-    public function createArticle(array $data){
+class ArticleRepository
+{
+    public function createArticle(array $data)
+    {
         return Article::create($data);
+    }
+    public function getAllArticleByWriterId(string $writer_id)
+    {
+        return Article::where("writer_id", $writer_id)->get();
     }
 }
