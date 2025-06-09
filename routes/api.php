ce9d714a5aa745341d17f9f8d7a6a28d519eebb8
Farham Zaker <farham.zaker007@gmail.com>
2025-06-08 12:04:08 -1200

    feat(article): add '/article/create' route

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,15 @@
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\isLogged;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 
 Route::prefix("auth")->group(function () {
     Route::post("/register", [UserController::class, "register"]);
     Route::post("/login", [UserController::class, "login"]);
     Route::middleware(isLogged::class)->get("/me", [UserController::class, "me"]);
+});
+
+Route::prefix("article")->group(function () {
+    Route::middleware(isLogged::class)->post("/create");
 });
