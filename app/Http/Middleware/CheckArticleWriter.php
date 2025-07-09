d37ee8e354fd585644257775f6f202981c643bbb
Farham <farham.zaker007@gmail.com>
2025-07-09 12:12:14 +0330

    refactor: update CheckArticleWriter middleware

--- app/Http/Middleware/CheckArticleWriter.php
+++ app/Http/Middleware/CheckArticleWriter.php
@@ -3,10 +3,12 @@
 namespace App\Http\Middleware;
 
 use App\Http\Helpers\ApiResponse;
+use App\Models\Article;
 use App\Repositories\ArticleRepository;
 use App\Repositories\UserRepository;
 use Closure;
 use Illuminate\Http\Request;
+use Mockery\Undefined;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckArticleWriter
@@ -19,7 +21,14 @@ class CheckArticleWriter
     public function handle(Request $request, Closure $next): Response
     {
         $authToken = $request->header("Authorization");
-        $article_id = $request->input("article_id");
+
+        // Try to get article_id from body or route params
+        $article_id = $request->input("article_id") ?? $request->route("article_id");
+
+        if (!$article_id) {
+            // If article_id is not provided at all
+            return ApiResponse::error(400, "You must send article id.");
+        }
 
         $userRepo = new UserRepository();
         $articleRepo = new ArticleRepository();
@@ -29,6 +38,7 @@ class CheckArticleWriter
 
         // Find the article by id of article
         $article = $articleRepo->getOneArticleById($article_id);
+        if (!$article) return ApiResponse::error(404, "There is no any article with such article id.");
 
         // Check if logged in user is writer of this article or not
         if ($article->writer_id != $user->id) return ApiResponse::error(403, "You are not writer of this article.");
